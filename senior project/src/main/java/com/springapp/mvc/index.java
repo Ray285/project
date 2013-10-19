c1e018a0c9b191f58198fe6c778b20bdd8397764
da314pc <ed2568@wayne.edu>
2013-10-19 18:41:19 -0400

    First Commit
    project in intellij

---
+++ senior project/src/main/java/com/springapp/mvc/index.java
@@ -0,0 +1,22 @@
+package com.springapp.mvc;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: User
+ * Date: 10/19/13
+ * Time: 6:03 PM
+ * To change this template use File | Settings | File Templates.
+ */
+
+@Controller
+@RequestMapping("/")
+public class index {
+}
---
+++ senior project/target/senior_project/WEB-INF/pages/index.jsp
@@ -0,0 +1,14 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+
+<!doctype>
+<html>
+<head>
+    <title>Home</title>
+</head>
+<body>
+<div>
+    <h1>Home</h1>
+
+</div>
+</body>
+</html>
